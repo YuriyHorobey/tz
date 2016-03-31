f5508188a1d86c5ad31dfe605d9367d859dcdf92
Yuriy Horobey <yuriy.horobey@gmail.com>
2016-03-31 16:38:15 +0300

    Dataservice in place, List displayed, some styling

--- flight-status/work/src/components/search/search.component.ts
+++ flight-status/work/src/components/search/search.component.ts
@@ -1,11 +1,24 @@
 /**
  * Created by yuriy.horobey on 30-Mar-16.
  */
+import {DataProviderService}  from '../../services/data.provider';
 import { Component } from '../../node_modules/angular2/core';
 @Component({
     selector: 'search',
-    templateUrl: '/components/search/search.view.html'
+    templateUrl: '/components/search/search.view.html',
+    providers: [DataProviderService]
 })
 export class SearchComponent {
     title = 'Search Flight Status';
+    flights;
+
+    constructor(private _dataProvider:DataProviderService) {
+        this.flights = _dataProvider.getData();
+    }
+
+    getStatusCSS(flight) {
+        var status = flight.status.toLowerCase();
+        var ret = 'results-table__data-cell_status-' + status;
+        return ret;
+    }
 }

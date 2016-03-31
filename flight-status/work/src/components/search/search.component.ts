0b8a4f74d1040ce8493325411108f164e2aae72f
Yuriy Horobey <yuriy.horobey@gmail.com>
2016-03-31 23:14:27 +0300

    Sorting on all fields done

--- flight-status/work/src/components/search/search.component.ts
+++ flight-status/work/src/components/search/search.component.ts
@@ -11,6 +11,8 @@ import {FORM_DIRECTIVES,ControlGroup,Control} from '../../node_modules/angular2/
     directives: [FORM_DIRECTIVES]
 })
 export class SearchComponent {
+
+    private _sort:Object;
     title = 'Search Flight Status';
 
     searchForm:ControlGroup;
@@ -19,6 +21,7 @@ export class SearchComponent {
 
     constructor(private _dataProvider:DataProviderService) {
         this.flights = _dataProvider.getData();
+        this._sort = {field: 'date', direction: 'up'}
         this.searchForm = new ControlGroup({
             code: new Control("", function validateCodeFiled() {
                 console.log(arguments)
@@ -35,9 +38,40 @@ export class SearchComponent {
         return ret;
     }
 
+    isButtonActiveCSS(button, direction) {
+        var ret =
+            ('' + button ).trim() == this._sort.field
+            &&
+            ('' + direction ).toLowerCase().trim == this._sort.direction
+                ? 'active' : '';
+        return ret;
+    }
+
+    onSortBy(filed:string, direction:string) {
+        this._sort.field = ('' + filed ).trim();
+        this._sort.direction = ('' + direction ).trim();
+        this._reorderFlights();
+    }
+
     onSubmit(form) {
         console.log('submit', form);
         this.flights = this._dataProvider.getDataFiltered(form.code, form.number);
         return false;
+    }
+
+    private _reorderFlights() {
+        var _sort:Object = this._sort;
+        this.flights = this.flights.sort(function (e1, e2) {
+            var ret = 0;
+            if (e1[_sort.field] < e2[_sort.field]) {
+                ret = -1;
+            } else if (e1[_sort.field] > e2[_sort.field]) {
+                ret = 1;
+            }
+            if (_sort.direction == 'down') {
+                ret = -ret;
+            }
+            return ret;
+        });
     }
 }
